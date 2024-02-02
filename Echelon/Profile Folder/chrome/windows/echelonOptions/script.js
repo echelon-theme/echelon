2e21317e7b7062154196a6701821e655907828ef
Daylin Cooper <dcoop2004@gmail.com>
2024-02-01 20:28:16 -0700

    Revise options panel for restartless

--- Echelon/Profile Folder/chrome/windows/echelonOptions/script.js
+++ Echelon/Profile Folder/chrome/windows/echelonOptions/script.js
@@ -69,9 +69,94 @@ function getTrueWinVer()
     return "Failed";
 }
 
+const OS_STYLE_MAP = {
+    "winxp": {
+        "Echelon.Appearance.XP": true,
+        "Echelon.Appearance.Australis.Windows10": false,
+        "Echelon.Appearance.Blue": false
+    },
+    "win7": {
+        "Echelon.Appearance.XP": false,
+        "Echelon.Appearance.Australis.Windows10": false
+        // blue can be variable
+    },
+    "win8": {
+        "Echelon.Appearance.XP": false,
+        "Echelon.Appearance.Australis.Windows10": false,
+        "Echelon.Appearance.Blue": false
+    },
+    "win10": {
+        "Echelon.Appearance.XP": false,
+        "Echelon.Appearance.Australis.Windows10": true,
+        "Echelon.Appearance.Blue": false
+    }
+};
+
+function osStyleGet()
+{
+    let style = null;
+    for (const osName in OS_STYLE_MAP)
+    {
+        const defs = OS_STYLE_MAP[osName];
+        for (const prop in defs)
+        {
+            if (tryGetBoolPref(prop) == defs[prop])
+            {
+                style = osName;
+            }
+            else
+            {
+                style = null;
+            }
+        }
+
+        if (style != null)
+            break;
+    }
+
+    return style ?? "win7"; // default
+}
+
+function osStyleSet(osName)
+{
+    for (const prop in OS_STYLE_MAP[osName])
+    {
+        trySetBoolPref(prop, OS_STYLE_MAP[osName][prop]);
+    }
+}
+
+// Initialise display of native controls patch:
+let ncpQueryResult = window.matchMedia("(-moz-ev-native-controls-patch)");
+if (!ncpQueryResult.matches)
+{
+    document.querySelector("#section-native-controls").style.display = none;
+}
+
+function refreshViewProperties()
+{
+    // Handle local display changes when the user changes configuration.
+    let restartRequired = isRestartRequired();
+
+    document.querySelector(".restart-required-label").style.display = restartRequired ? "inline" : "none";
+
+    for (const elm of document.querySelectorAll(".option[australis-only]"))
+    {
+        let localStyle = document.querySelector(".option[data-option='Echelon.Appearance.Style']").value;
+        elm.disabled = localStyle != 4;
+    }
+
+    for (const elm of document.querySelectorAll(".option[win7-only]"))
+    {
+        let localOS = document.querySelector(".option.os-control-list").value;
+        elm.disabled = localOS != "win7";
+    }
+}
+
+document.querySelector(".option.os-control-list").value = osStyleGet();
+
 /* Fill current values */
 for (const option of document.querySelectorAll(".option"))
-{   
+{
     switch (option.dataset.type)
     {
         case "bool":
@@ -85,33 +170,107 @@ for (const option of document.querySelectorAll(".option"))
             option.value = tryGetStringPref(option.dataset.option);
             break;
     }
+    option.originalValue = getOptionValue(option);
+
+    if (option.localName == "menulist")
+        option.addEventListener("command", refreshViewProperties);
+    else if (option.localName == "checkbox")
+        option.addEventListener("CheckboxStateChange", refreshViewProperties);
 }
 
-/* Events */
-document.getElementById("ok-button").addEventListener("click", function()
+refreshViewProperties();
+
+function getOptionValue(optElm)
 {
+    switch (optElm.dataset.type)
+    {
+        case "bool":
+            return optElm.checked;
+        case "int":
+        case "enum":
+            return optElm.value;
+        case "string":
+            return optElm.value;
+    }
+
+    return null;
+}
+
+function isRestartRequired()
+{
+    let restartRequired = false;
+
     for (const option of document.querySelectorAll(".option"))
-    {   
-        switch (option.dataset.type)
+    {
+        if (option.closest("[section-change-requires-restart]") || option.getAttribute("change-requires-restart"))
         {
-            case "bool":
-                trySetBoolPref(option.dataset.option, option.checked);
-                break;
-            case "enum":
-                trySetIntPref(option.dataset.option, Number(option.value));
-                break;
-            case "int":
-                trySetIntPref(option.dataset.option, Math.floor(Number(option.value)));
-                break;
-            case "string":
-                trySetStringPref(option.dataset.option, option.value);
-                break;
+            if (option.originalValue != getOptionValue(option))
+            {
+                restartRequired = true;
+            }
         }
     }
 
-    _ucUtils.restart(true);
-    window.close();
-});
+    return restartRequired;
+}
+
+function okApplyHandler(e, closeWindow = false)
+{
+    let restartRequired = isRestartRequired();
+
+    let restartStruct ={
+        accepted: false,
+        icon: "warning",
+        title: "Are you sure?",
+        message: "You have changed options that require a restart. Do you wish to restart and apply the changes?",
+        acceptButtonText: "Restart"
+    };
+
+    if (restartRequired)
+    {
+        window.openDialog(
+            "chrome://userchrome/content/windows/echelonOptions/dialog.xhtml",
+            "Are you sure?",
+            "chrome,centerscreen,resizeable=no,dependent,modal",
+            restartStruct
+        );
+    }
+
+    if (!restartRequired || restartStruct.accepted)
+    {
+        for (const option of document.querySelectorAll(".option"))
+        {
+            switch (option.dataset.type)
+            {
+                case "bool":
+                    trySetBoolPref(option.dataset.option, option.checked);
+                    break;
+                case "enum":
+                    trySetIntPref(option.dataset.option, Number(option.value));
+                    break;
+                case "int":
+                    trySetIntPref(option.dataset.option, Math.floor(Number(option.value)));
+                    break;
+                case "string":
+                    trySetStringPref(option.dataset.option, option.value);
+                    break;
+            }
+        }
+
+        // OS style needs to be applied separately
+        osStyleSet(document.querySelector(".option.os-control-list").value);
+
+        if (restartRequired)
+            _ucUtils.restart(true);
+
+        if (closeWindow)
+            window.close();
+    }
+}
+
+/* Events */
+document.getElementById("ok-button").addEventListener("click", e => okApplyHandler(e, true));
+document.getElementById("apply-button").addEventListener("click", e => okApplyHandler(e, false));
 
 document.getElementById("cancel-button").addEventListener("click", function()
 {
