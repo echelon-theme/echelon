0ede809d4223fef88c8e50371c2113346e7c22f5
0penrc <80153347+0penrc@users.noreply.github.com>
2024-02-08 18:52:12 +0100

    Update echelon_pages.uc.js

--- Echelon/Profile Folder/chrome/JS/echelon_pages.uc.js
+++ Echelon/Profile Folder/chrome/JS/echelon_pages.uc.js
@@ -122,7 +122,7 @@ EchelonPageManager.registerAboutPage(
 );
 EchelonPageManager.registerAboutPage(
     "privatebrowsing",
-    "chrome://userchrome/content/pages/privatebrowsing/privatebrowsing.xhtml"
+    "chrome://userchrome/content/pages/privateBrowsing/privatebrowsing.xhtml"
 );
 EchelonPageManager.registerAboutPage(
     "config",
