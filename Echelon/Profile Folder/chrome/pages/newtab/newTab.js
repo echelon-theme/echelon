d7c5aba8ecccaaa54ab639584fedac8b23eb718c
aubymori <aubyomori@gmail.com>
2024-04-03 18:34:13 -0500

    Update NCP link and etc.

--- Echelon/Profile Folder/chrome/pages/newtab/newTab.js
+++ Echelon/Profile Folder/chrome/pages/newtab/newTab.js
@@ -20,9 +20,46 @@ const gPage = {
   set enabled(aValue) {
     Services.prefs.setBoolPref(PREF_NEWTAB_ENABLED, !!aValue);
   },
+
+  _updateAttributes: function Page_updateAttributes(aValue) {
+    // Set the nodes' states.
+    let nodeSelector = "#newtab-scrollbox, #newtab-toggle, #newtab-grid, #newtab-search-container";
+    for (let node of document.querySelectorAll(nodeSelector)) {
+      if (aValue)
+        node.removeAttribute("page-disabled");
+      else
+        node.setAttribute("page-disabled", "true");
+    }
+
+    // Enables/disables the control and link elements.
+    let inputSelector = ".newtab-control, .newtab-link";
+    for (let input of document.querySelectorAll(inputSelector)) {
+      if (aValue) 
+        input.removeAttribute("tabindex");
+      else
+        input.setAttribute("tabindex", "-1");
+    }
+
+    // Update the toggle button's title.
+    let toggle = document.getElementById("newtab-toggle");
+    toggle.setAttribute("title", newTabString(aValue ? "hide" : "show"));
+  },
+
+  handleEvent: function Page_handleEvent(aEvent) {
+    switch (aEvent.type){
+      case "click":
+        let {button, target} = aEvent;
+        if (target.id == "newtab-toggle") {
+          if (button == 0) {
+            this.enabled = !this.enabled;
+          }
+          break;
+        }
+    }
+  },
   
   init: function Page_init() {
-
+    this._updateAttributes(this.enabled);
   }
 };
 
