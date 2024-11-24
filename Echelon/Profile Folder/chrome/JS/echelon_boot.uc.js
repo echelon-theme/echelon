6925ada4976b7a26ab0fec6593b713e0630b13a1
travis <94665268+travy-patty@users.noreply.github.com>
2024-11-23 19:37:39 -0800

    start codebase
    Starting development on a rewrite of Echelon, making the codebase much more cleaner. This rewrite is going to be designed for the Marble (https://github.com/Erizur/marble) fork

--- Echelon/Profile Folder/chrome/JS/echelon_boot.uc.js
+++ Echelon/Profile Folder/chrome/JS/echelon_boot.uc.js
@@ -3,57 +3,18 @@
 // @description 	Initializes Echelon modules for different pages.
 // @author			aubymori
 // @include			(.*)
-// @include         chrome://global/content/commonDialog.xhtml
-// @loadOrder       14
+// @loadOrder       0
 // ==/UserScript==
-
-/* ^ We can safely include everything; the script only injects into pages which have chrome privileges */
 
 let ECHELON_BOOT_CONFIG = {
 	/* Main browser window */
 	"chrome://browser/content/browser.xhtml": {
-		updates: true,
-		wizard: true,
 		themes: {
 			style: true,
-			channel: true,
 			bools: [
-				"Echelon.Appearance.Blue",
-				"Echelon.Appearance.Australis.EnableFog",
-				"Echelon.Appearance.Australis.Windows8",
-				"Echelon.Appearance.Australis.Windows10",
-				"Echelon.Appearance.XP",
-				"Echelon.Appereance.Linux",
-				"Echelon.FirefoxButton.CustomStyle",
-				"Echelon.Appearance.eXPerienceLunaMsstylesFixes",
-				"Echelon.Appearance.NewLogo",
-				"Echelon.Option.HideUnifiedExtensions"
+				"Echelon.Appearance.Blue"
 			]
 		},
-		nativeControls: true
-	},
-	/* Options */
-	"about:preferences": {
-		themes: { style: true }
-	},
-	/* Add-ons Manager */
-	"about:addons": {
-		themes: { style: true }
-	},
-	/* Echelon Options page */
-	"about:echelon": {
-		themes: { style: true }
-	},
-	/* "About Mozilla Firefox" dialog */
-	"chrome://browser/content/aboutDialog.xhtml": {
-		themes: {
-			style: true,
-			channel: true,
-			bools: ["Echelon.Appearance.NewLogo"]
-		}
-	},
-	/* "Delete Recent History" dialog */
-	"chrome://browser/content/sanitize.xhtml": {
 		nativeControls: true
 	}
 };
@@ -61,35 +22,12 @@ let ECHELON_BOOT_CONFIG = {
 {
 	function bootEchelon(context, config)
 	{
-		if (config?.updates)
-		{
-			let { EchelonUpdateChecker } = ChromeUtils.importESModule("chrome://modules/content/EchelonUpdateChecker.sys.mjs");
-			EchelonUpdateChecker.setWindow(context);
-			EchelonUpdateChecker.checkForUpdate();
-		}
-
 		if (config?.themes)
 		{
 			let { EchelonThemeManager } = ChromeUtils.importESModule("chrome://modules/content/EchelonThemeManager.sys.mjs");
 			context.g_themeManager = new EchelonThemeManager;
 			context.g_themeManager.init(context.document.documentElement, config.themes);
 		}
-
-		if (config?.wizard)
-		{
-			let { openEchelonWizardWindow } = ChromeUtils.import("chrome://userscripts/content/echelon_wizard.uc.js");
-			openEchelonWizardWindow = openEchelonWizardWindow.bind(context);
-			openEchelonWizardWindow(true);
-
-			context.addEventListener("echelon-reopen-wizard", function(e)
-			{
-				// Kill the wizard notification early. Technically, it will disappear as soon as this
-				// callback ends, but it looks bad.
-				gBrowser?.getNotificationBox()?.getNotificationWithValue("echelon-setup-closed")?.dismiss();
-				openEchelonWizardWindow(false);
-			});
-		}
-
 		if (config?.nativeControls)
 		{
 			let { NativeControls } = ChromeUtils.importESModule("chrome://modules/content/NativeControls.sys.mjs");
--- Echelon/Profile Folder/chrome/_echelon_watch_build.cmd
+++
@@ -1,1 +0,0 @@
-_echelon_build watch
