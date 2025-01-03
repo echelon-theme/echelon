7ece60b3650cea457318d835640a09381ceebc3c
travis <94665268+travy-patty@users.noreply.github.com>
2025-01-03 01:38:05 -0800

    add ff14 about:home style & functionality of custom snippets
    custom snippets will let me show custom stuff on the homepage like how mozilla did

--- Echelon/Profile Folder/chrome/pages/home/home.js
+++ Echelon/Profile Folder/chrome/pages/home/home.js
@@ -10,62 +10,104 @@ let titles = BrandUtils.getDefaultTitles();
 
 function createHomePage() {
     let container = document.body;
-    let homeFragment = null;
 
-    if (!style && style == 0) {
-        homeFragment = `
-            <div id="brandStartSpacer" />
+    homeFragment = `
+        <div id="brandStartSpacer" />
 
-                <div id="brandStart">
-                <img id="brandStartLogo" alt="" />
-                </div>
+            <div id="brandStart">
+            <img id="brandStartLogo" alt="" />
+            </div>
 
-                <div id="searchContainer">
-                <form method="get" name="searchForm" id="searchForm" onsubmit="onSearchSubmit(event)">
-                    <div id="searchLogoContainer"><img id="searchEngineLogo" /></div>
-                    <div id="searchInputContainer">
-                    <input type="text" name="q" value="" id="searchText" maxlength="256" placeholder="" />
+            <div id="searchContainer">
+            <html:form method="get" name="searchForm" id="searchForm" onsubmit="onSearchSubmit(event)">
+                <div id="searchLogoContainer"><img id="searchEngineLogo" /></div>
+                <div id="searchInputContainer">
+                <html:input type="text" name="q" value="" id="searchText" maxlength="256" placeholder="" />
 
-                    </div>
-                    <div id="searchButtons">
-                    <input id="searchSubmit" type="submit" value="${homeBundle.GetStringFromName("searchEngineButton")}" />
-                    </div>
-                </form>
                 </div>
+                <div id="searchButtons">
+                <html:input id="searchSubmit" type="submit" value="${homeBundle.GetStringFromName("searchEngineButton")}" />
+                </div>
+            </html:form>
+            </div>
 
-                <div id="contentContainer">
-                <div id="snippetContainer">
-                    <div id="defaultSnippets" hidden="true">
-                        <span id="defaultSnippet1">${homeBundle.formatStringFromName("snippet_1", [product])}</span>
-                        <span id="defaultSnippet2">${homeBundle.formatStringFromName("snippet_2", [product])}</span>
-                        <span id="snippetEchelon">${homeBundle.GetStringFromName("snippet_echelon")}</span>
-                    </div>
-                    <div id="snippets"/>
+            <div id="contentContainer">
+            <div id="snippetContainer">
+                <div id="defaultSnippets" hidden="true">
+                    <span id="defaultSnippet1">${homeBundle.formatStringFromName("snippet_1", [product])}</span>
+                    <span id="defaultSnippet2">${homeBundle.formatStringFromName("snippet_2", [product])}</span>
+                    <span id="snippetEchelon">${homeBundle.GetStringFromName("snippet_echelon")}</span>
                 </div>
+                <div id="snippets"/>
+            </div>
 
-                <div id="launcher">
-                    <button id="restorePreviousSession" onclick="restoreLastSession()">${homeBundle.GetStringFromName("restoreLastSessionButton")}</button>
-                </div>
-                </div>
+            <div id="launcher">
+                <button id="restorePreviousSession" onclick="restoreLastSession()">${homeBundle.GetStringFromName("restoreLastSessionButton")}</button>
+            </div>
+            </div>
 
-                <div id="bottomSection">
-                <div id="aboutMozilla">
-                    <html:a href="http://www.mozilla.com/about/">${homeBundle.GetStringFromName("aboutMozilla")}</html:a>
-                </div>
-                </div>
-        `
-    }
+            <div id="bottomSection">
+            <div id="aboutMozilla">
+                <html:a href="http://www.mozilla.com/about/">${homeBundle.GetStringFromName("aboutMozilla")}</html:a>
+            </div>
+        </div>
+    `
+
+    stylesheet = `
+        <html:link rel="stylesheet" href="chrome://userchrome/content/pages/home/home-old.css" />
+    `
     
-    if (style == 1) {
+    if (style >= 1) {
         homeFragment = `
-            weenor
+            <div class="spacer"/>
+            <div id="topSection">
+            <html:img id="brandLogo" alt=""/>
+
+            <div id="searchContainer">
+                <html:form name="searchForm" id="searchForm" onsubmit="onSearchSubmit(event)">
+                <div id="searchLogoContainer"><img id="searchEngineLogo"/></div>
+                <html:input type="text" name="q" value="" id="searchText" maxlength="256"
+                        autofocus="autofocus"/>
+                <html:input id="searchSubmit" type="submit" value="${homeBundle.GetStringFromName("searchEngineButton")}"/>
+                </html:form>
+            </div>
+
+            <div id="snippetContainer">
+                <div id="defaultSnippets" hidden="true">
+                    <span id="defaultSnippet1">${homeBundle.formatStringFromName("snippet_1", [product])}</span>
+                    <span id="defaultSnippet2">${homeBundle.formatStringFromName("snippet_2", [product])}</span>
+                    <span id="snippetEchelon">${homeBundle.GetStringFromName("snippet_echelon")}</span>
+                </div>
+                <div id="snippets"/>
+            </div>
+            </div>
+            <div class="spacer"/>
+
+            <div id="launcher" session="true">
+            <button class="launchButton" id="downloads">${homeBundle.GetStringFromName("downloadsButton")}</button>
+            <button class="launchButton" id="bookmarks">${homeBundle.GetStringFromName("bookmarksButton")}</button>
+            <button class="launchButton" id="history">${homeBundle.GetStringFromName("historyButton")}</button>
+            <button class="launchButton" id="addons">${homeBundle.GetStringFromName("addonsButton")}</button>
+            <button class="launchButton" id="sync">${homeBundle.GetStringFromName("syncButton")}</button>
+            <button class="launchButton" id="settings">${homeBundle.GetStringFromName("settingsButton")}</button>
+            <div id="restorePreviousSessionSeparator"/>
+            <button class="launchButton" id="restorePreviousSession">${homeBundle.GetStringFromName("restoreLastSessionButton")}</button>
+            </div>
+
+            <a id="aboutMozilla" href="${homeBundle.GetStringFromName("aboutMozilla")}"/>
+        `
+
+        stylesheet = `
+            <html:link rel="stylesheet" href="chrome://userchrome/content/pages/home/home-new.css" />
         `
     }
 
+    document.head.appendChild(MozXULElement.parseXULToFragment(stylesheet));
     container.appendChild(MozXULElement.parseXULToFragment(homeFragment));
 
     document.querySelector("#defaultSnippet1 a").href = "https://www.mozilla.org/firefox/features/?utm_source=snippet&utm_medium=snippet&utm_campaign=default+feature+snippet";
     document.querySelector("#defaultSnippet2 a").href = "https://addons.mozilla.org/firefox/?utm_source=snippet&utm_medium=snippet&utm_campaign=addons";
+    document.querySelector("#snippetEchelon a").href = "https://github.com/echelon-theme/echelon/issues";
 
     Services.search.getDefault().then(engine => {
 		window.engine = engine;
@@ -79,7 +121,39 @@ function createHomePage() {
 	});
 
     updateHomepageStyle();
-    showSnippets();
+    insertCustomSnippets();
+}
+
+function insertCustomSnippets()
+{
+    const snippetsURL = "https://rawcdn.githack.com/echelon-theme/echelon-theme.github.io/0d2cc67936690454e8bea88ac488c478d8e889d1/snippets.json";
+
+    fetch(snippetsURL, {method: "Get"})
+    .then((response) => response.json())
+
+    .then((data) => {
+        for (const snippet of data.snippets) {
+            let snippetElem = document.createElement("span");
+
+            snippetElem.id = snippet.id;
+            snippetElem.classList.add("customSnippet");
+            snippetElem.style.setProperty("background-image", `url(${snippet.image})`);
+            snippetElem.innerHTML = snippet.text;
+
+            if (snippet.link) {
+                snippetElem.querySelector("a").href = snippet.link;
+            }
+
+            document.getElementById("defaultSnippets").appendChild(snippetElem);
+        }
+
+        showSnippets();
+    })
+
+    .catch(error => {
+        console.error("Can't fetch unique snippets, show default:", error);
+        showSnippets();
+    });
 }
 
 function showSnippets()
